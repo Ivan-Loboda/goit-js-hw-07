2b24e852fe76aae34a86ba2ae9af6119df2d399b
Ivan-Loboda <Ivan.Loboda.1996@gmail.com>
2021-09-28 18:12:40 +0300

    2 часть с ошибкой

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryBox = document.querySelector('.gallery');
+
+const markUp = galleryItems.map(item =>
+    `<li>
+    <a class="gallery__link" href="${item.original}" style ="cursor: default">
+                <img class="gallery__image"
+                    src="${item.preview}"
+                    alt="${item.description}"
+                />
+            </a>
+    </li>`).join("");
+
+
+galleryBox.insertAdjacentHTML('beforeend', markUp);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
