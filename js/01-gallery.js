db5f2e3f288d7267f50bc2bee2c4e8745f4ca7c8
Ivan-Loboda <Ivan.Loboda.1996@gmail.com>
2021-09-28 17:19:20 +0300

    1 часть

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryBox = document.querySelector('.gallery');
+
+const markUp = galleryItems.map(item =>
+    `<div class="gallery__item">
+            <a class="gallery__link" href="${item.original}" style ="cursor: default">
+                <img class="gallery__image"
+                    src="${item.preview}"
+                    data-source="${item.original}"
+                    alt="${item.description}"
+                />
+            </a>
+        </div>`)
+    .join("");
+
+galleryBox.insertAdjacentHTML('beforeend', markUp);
+
+galleryBox.addEventListener('click', onClick);
+
+let instance
+
+function onClick(event) {
+    event.preventDefault();
+
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+
+    instance = basicLightbox.create(`<img src="${event.target.dataset.source}">`)
+
+    instance.show()
+}
+
+document.addEventListener("keydown", event => {
+
+    if (!instance && !instance.visible()) return false;
+
+    if (event.code === "Escape") {
+        instance.close()
+    }
+});
